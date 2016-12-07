2a2ec7f1c117131bf27f3d70be11721173d01916
Alexander Harchenko <morontt@gmail.com>
2016-12-08 00:46:46 +0200

    add page parameter in route, add offset

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -3,4 +3,4 @@
 SELECT `p`.`title`, `p`.`text_post`, `p`.`url`, `p`.`time_created`, `c`.`name` AS `cat_name`, `c`.`url` AS `cat_url`
 FROM `posts` AS `p`
 INNER JOIN `category` AS `c` ON c.id = p.category_id
-WHERE (p.hide = 0) ORDER BY `p`.`time_created` DESC LIMIT 10
+WHERE (p.hide = 0) ORDER BY `p`.`time_created` DESC LIMIT 10 OFFSET :offset
--- src/clj/progl/routes/home.clj
+++ src/clj/progl/routes/home.clj
@@ -5,13 +5,13 @@
             [progl.db.core :as db]
             [clojure.java.io :as io]))
 
-(defn home-page []
-  (layout/render "home.html" {:topics (db/get-visible-posts)}))
+(defn home-page [page]
+  (layout/render "home.html" {:topics (db/get-visible-posts {:offset (* 10 (- page 1))})}))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" [] (home-page 1))
+  (GET ["/:page" :page #"\d+"] [page] (home-page (Integer/parseInt page)))
   (GET "/about" [] (about-page)))
-
